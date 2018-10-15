2233bf09f2d146142d38bd60493f0446c192a995
Brian O'Neill <C00213756@itcarlow.ie>
2018-10-15 15:00:35 +0100

    changed level size and added moving ship

---
+++ Lab3-B/Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(moveHorizontal, 2.1f, -40);
+
+        rb.velocity = move * speed;
+
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f
+          
+            );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+
+}
+
