3ee66b5b24d0eb303e62c1061030d178ac4caaab
Aoife <c00218374@itcarlow.ie>
2018-10-15 11:31:50 +0100

    Script folder

---
+++ Lab3-B/Assets/Script/EnemyMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class EnemyMover : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+    public float speed;
+    public Boundary boundary;
+
+	// Use this for initialization
+	void FixedUpdate () {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.right * -speed;
+        if(rigidBody.position.x > boundary.xMax)
+        {
+            rigidBody.velocity = transform.right * speed;
+        }
+        else if (rigidBody.position.x < boundary.xMin)
+        {
+            rigidBody.velocity = transform.right * -speed;
+        }
+    }
+}
